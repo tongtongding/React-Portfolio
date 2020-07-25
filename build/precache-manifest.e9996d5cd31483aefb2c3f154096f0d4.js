b683abe071e2fa069ea848e96f44adddd2656004
tongtong ding <tding7@asu.edu>
2020-07-25 14:18:58 -0700

    background img

--- build/precache-manifest.e9996d5cd31483aefb2c3f154096f0d4.js
+++ build/precache-manifest.e9996d5cd31483aefb2c3f154096f0d4.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d50112f0ff0c8833e59c34e4366be2f1",
+    "revision": "dd564f20a0f22a6b68056ca8996d91eb",
     "url": "/React-Portfolio/index.html"
   },
   {
-    "revision": "0865e3b68a80c7c529a4",
-    "url": "/React-Portfolio/static/css/main.b7199ada.chunk.css"
+    "revision": "d86193898df33586bf28",
+    "url": "/React-Portfolio/static/css/main.22fd349e.chunk.css"
   },
   {
     "revision": "840118e27f526dd4f2a2",
@@ -16,11 +16,15 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/React-Portfolio/static/js/2.b7331160.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "0865e3b68a80c7c529a4",
-    "url": "/React-Portfolio/static/js/main.89e9a55e.chunk.js"
+    "revision": "d86193898df33586bf28",
+    "url": "/React-Portfolio/static/js/main.edbef638.chunk.js"
   },
   {
     "revision": "bcebe2d38e8b197ca4b9",
     "url": "/React-Portfolio/static/js/runtime-main.266d4783.js"
+  },
+  {
+    "revision": "f19f0f949cd90592eb15e4b4051f2474",
+    "url": "/React-Portfolio/static/media/background.f19f0f94.jpg"
   }
 ]);
--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import Home from "./components/home/Home";
 import Portfolio from "./components/portfolio/Portfolio";
 import Contact from "./components/contact/Contact";
@@ -13,7 +13,7 @@ function App() {
   return (
 
     
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router>
       
       <Navbar />
       <br />
